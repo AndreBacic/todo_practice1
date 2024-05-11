4651a330b564f80d8182f658808352e0cc69ddb4
AndreBacic <andre9050@yahoo.com>
2024-05-10 21:24:13 -0400

    Read todos

--- app/todos/[id]/page.tsx
+++ app/todos/[id]/page.tsx
@@ -1,17 +1,22 @@
+import { RecordModel } from "pocketbase";
 
 async function getTodo(id: string) {
-    const response = await fetch(`url/${id}`);
+    // TODO: refactor pocketbase url to be in a global setting somewhere
+    const response = await fetch(`http://127.0.0.1:8090/api/collections/todos/records/${id}`);
     const data = await response.json()
-    return data
+    return data as RecordModel
 }
 
-export default function Todo({ params }: any) {
-    const todo = getTodo(params.id);
+export default async function Todo({ params }: any) {
+    const todo = await getTodo(params.id);
 
     return (
         <div>
-            <h1>{todo.title}</h1>
+            <h2>{todo.title}</h2>
             <p>{todo.description}</p>
+            <p>Priority: {todo.priority}</p>
+            <p>Due {todo.deadline}</p>
+            <p>Created {todo.created}</p>
         </div>
     )
 }
--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,8 +1,28 @@
+import Link from 'next/link';
+import PocketBase from 'pocketbase';
 
-export default function TodosList() {
+async function getTodos() {
+    const db = new PocketBase('http://127.0.0.1:8090')
+    const data = await db.collection('todos').getList(0, 24)
+    console.log(data)
+
+    return data?.items
+}
+
+export default async function TodosList() {
+    const todos = await getTodos()
+
     return (
         <div>
-            <h1>This is where the Todos will be listed...</h1>
+            {todos?.map((todo) => (
+                <Link key={todo.id} href={`/todos/${todo.id}`}>
+                    <h2>{todo.title}</h2>
+                    <p>{todo.description}</p>
+                    <p>Priority: {todo.priority}</p>
+                    <p>Due {todo.deadline}</p>
+                    <p>Created {todo.created}</p>
+                </Link>
+            ))}
         </div>
     )
 }
