2286fc6cea9d659a20504a7f339154847ff473c8
AndreBacic <andre9050@yahoo.com>
2025-04-10 13:00:45 -0400

    Add launch.json

--- app/todos/[id]/edit/page.tsx
+++ app/todos/[id]/edit/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 export default async function({params}: {params: {id: string}}) {
     const db = new PocketBase(process.env.POCKETBASE_URL);
     const todo = await db.collection<Todo>("todos").getOne(params.id);
+    console.log("Edit todo: ", todo);
 
     async function updateTodo(formSubmission: FormData) {
         "use server";
@@ -14,6 +15,7 @@ export default async function({params}: {params: {id: string}}) {
             priority: formSubmission.get("priority"),
             deadline: formSubmission.get("deadline"),
         };
+        console.log("Hopefully changing todo to: ", data);
         await db.collection<Todo>("todos").update(params.id, data);
     };
 
@@ -21,7 +23,7 @@ export default async function({params}: {params: {id: string}}) {
         <div className="container">
             <Link href={`/todos/${todo.id}`} className="block pb-8">&lt;- Back to todo</Link>
             <h1>Edit {todo.title}</h1>
-            <form action={updateTodo}>
+            <form action={updateTodo} method="post">
                 <input type="text" name="title" defaultValue={todo.title} required />
                 <input type="text" name="description" defaultValue={todo.description} />
                 <input type="number" name="priority" defaultValue={todo.priority} />
